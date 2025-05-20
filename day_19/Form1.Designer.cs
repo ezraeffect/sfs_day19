eee85eedd708032df40b0e5ab1cf60392cd0b413
ezraeffect <build@fx0.kr>
2025-05-20 09:37:00 +0900

    add: BackgroundWorker 실습

--- day_19/Form1.Designer.cs
+++ day_19/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 149);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(12, 167);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 1;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 201);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button_start;
     }
 }
 
--- day_19/Form1.cs
+++ day_19/Form1.cs
@@ -6,15 +6,66 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using System.Runtime.Remoting.Channels;
 
 namespace day_19
 {
     public partial class Form1 : Form
     {
+        BackgroundWorker worker = new BackgroundWorker();
+
         public Form1()
         {
             InitializeComponent();
+
+
+            // 진행률 관련 옵션
+            worker.WorkerReportsProgress = true;
+
+            // 프로그래스 바 기본 범위 설정
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+
+            // 이벤트 연결
+            worker.DoWork += Worker_DoWork;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+
         }
+
+        // 작업 완료 시 MessageBox 출력
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            MessageBox.Show("완료됨");
+        }
+
+        // UI Thread (ProgressBar Value Update)
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        // DoWork가 실행할 메서드
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // throw new NotImplementedException();
+            for (int i = 0; i <= 100; i++)
+            {
+                Thread.Sleep(30); // 30ms 지연
+                worker.ReportProgress(i); // 진행 상황 보고
+            }
+        }
+
+        // Button 클릭 시, BGW 작업을 시작
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            worker.RunWorkerAsync();
+        }
+
+       
     }
 }
